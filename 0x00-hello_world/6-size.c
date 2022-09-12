26f78bfc6eff2ec7c4c4f769a2d66118b8d9ea99
musaabmoha <mohamusaab11@gmail.com>
2022-09-12 12:18:04 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int ainteger;
 long along;
 long long alonglong;
 float afloat;
-printf("Size of a char:% lu byte(s)\n", sizeof(1));
-printf("Size ofan int:% lu byte(s)\n", sizeof(4));
-printf("Size of a long int: % lu byte(s)\n", sizeof(8));
-printf("Size of a long long int: % lu byte(s)\n", sizeof(8));
-printf("Size of a float : % lu byte(s)\n", sizeof(4));
+printf("Size of a char:% zu byte(s)\n", sizeof(char));
+printf("Size ofan int:% zu byte(s)\n", sizeof(int));
+printf("Size of a long int: % zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: % zu byte(s)\n", sizeof(long long int));
+printf("Size of a float : % zu byte(s)\n", sizeof(float));
 return (0);
 }
