af01f0bc58e19359e7eee6da5ddfba17af46f180
musaabmoha <mohamusaab11@gmail.com>
2022-10-05 03:01:49 -0700

    0-create_array.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,7 @@
-#int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 
 char *create_array(unsigned int size, char c);
 
@@ -13,3 +16,5 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 
 char **strtow(char *str);
+
+#endif
